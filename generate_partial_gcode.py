93027b16982d619f7550af1268d8d30a46410a4a
arnaud162 <82218134+arnaud162@users.noreply.github.com>
2025-03-30 18:29:41 -0300

    Add files via upload

---
+++ generate_partial_gcode.py
@@ -0,0 +1,7 @@
+#1 get file. scans for gcode files, if many are found asks which one of them
+
+#2 asks for current height
+
+#3 get layer height
+
+#4 generates new gcode starting at height where print was interupted
